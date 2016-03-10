a8834b876f8837feb2ac04310efbb150cc2193ce
kjagla <kjagla@sointeractive.pl>
2016-03-10 11:33:22 +0100

    poprawione pathy do folderow

---
+++ filenameConst/hard/ex19.py
@@ -0,0 +1,14 @@
+
+def cheese_and_crackers(cheese_count, boxes_of_crackers):
+	print "You have %r cheeses" % cheese_count
+	print "You have %r boxes of crackers" % boxes_of_crackers
+
+
+
+print "we can use function directly"
+
+
+print "Ile chcesz sera ?"
+ileSera =raw_input() 
+
+cheese_and_crackers(ileSera,30)
---
+++ filenameConst/hardWay.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import csv
+
+print "lalalal"
+
+tabby_cat= "\tI'm tabbed in"
+
+print tabby_cat
+
+persian_cat = "I'm split\non a line."
+
+print persian_cat
+
+backslash_cat = "I'm \\ a \\cat."
+
+print backslash_cat
+print "karol\nJagla"
